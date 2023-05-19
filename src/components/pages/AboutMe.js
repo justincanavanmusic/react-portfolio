f9c244308cdc9fea409e6cc58e5b839f254da1dc
justincanavanmusic <121266850+justincanavanmusic@users.noreply.github.com>
2023-05-19 16:25:39 -0400

    started about-me animation

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './index.css';
 import photo from '../../assets/Screenshot 2023-03-28 at 5.44.08 PM.png'
+import '../../animation.css'
 
 const style = {
   photo: {
@@ -12,24 +13,16 @@ export default function AboutMe() {
   
     return (
         <>
-      <div className='container pb-1'>
-
-     
-      <h2 className='cream-font about-me'>About Me</h2>
-      
-      <img src={photo} style={style.photo} alt='Justin Canavan headshot' 
-      className="headshot mb-3"></img>
-      <hr className='opacity-75 w-75 mx-auto mb-4'></hr>
-      <div className='container'>
-        <div className='mx-auto mb-5 col-lg-7 col-md-9 col-sm-10 col-11'>
-
-      <p className='bio'>A passionate and hard-working <strong>Full-Stack</strong> software developer, proficient in Front-end and
-Back-end technologies ranging from JavaScript, React.js, Node.js, RESTful API’s, UI/UX design
-and more. Over a decade-long career in the music industry, I have honed my skills in the art of
-mastering a language, effective teamwork, meticulous attention to detail, allocating time
-efficiently in high-pressure situations, all while consistently delivering a high quality product.
-Known as a reliable professional who is always pushing to advance their craft. </p>
+      <div className='container text-center pb-1'>
+      <div className='d-flex justify-content-center'>
+      <h2 className='text-center abt-me-animation cream-font about-me'>Hi! I'm Justin Canavan!</h2>
       </div>
+      <img src={photo} style={style.photo} alt='Justin Canavan headshot' 
+      className="photo-anim headshot mb-3"></img>
+      {/* <hr className='opacity-75 w-75 mx-auto mb-4'></hr> */}
+      
+        <div className='mx-auto mb-5 col-lg-7 col-md-9 col-sm-10 col-11'>
+      
       </div>
       </div>
   
