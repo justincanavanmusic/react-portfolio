02e90210a1fa78d1fa8792735cfd34054f01ad1b
justincanavanmusic <121266850+justincanavanmusic@users.noreply.github.com>
2023-05-23 09:44:08 -0500

    typing animation

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -1,4 +1,4 @@
-import React from 'react';
+ import React from 'react';
 import './index.css';
 import photo from '../../assets/Screenshot 2023-03-28 at 5.44.08 PM.png'
 import '../../animation.css'
@@ -15,16 +15,29 @@ export default function AboutMe() {
         <>
       <div className='container text-center pb-1'>
       <div className='d-flex justify-content-center'>
-      <h2 className='text-center abt-me-animation cream-font about-me'>Hi! I'm Justin Canavan!</h2>
+      <h2 className='text-center abt-me-animation cream-font about-me'>Hi! I'm Justin Canavan.</h2>
       </div>
+
       <img src={photo} style={style.photo} alt='Justin Canavan headshot' 
       className="photo-anim headshot mb-3"></img>
       {/* <hr className='opacity-75 w-75 mx-auto mb-4'></hr> */}
-      
-        <div className='mx-auto mb-5 col-lg-7 col-md-9 col-sm-10 col-11'>
-      
+    
       </div>
+      <div></div>
+      <div class='wrapper'>
+        <div class='static-text'>
+          I'm a</div> 
+        <ul class='dynamic-text'>
+          <li><span>JavaScript Developer</span></li>
+          <li><span>React Developer</span></li>
+          {/* <li><span>RESTful API Consumer</span></li> */}
+          <li><span>Musician</span></li>
+          {/* <li><span>Brother, son, friend!</span></li> */}
+
+        </ul>
       </div>
+
+    
   
 
       </>
