9e36348151d82d04526566557c347f56991eabbc
justincanavanmusic <121266850+justincanavanmusic@users.noreply.github.com>
2023-05-30 17:15:31 -0500

    about me animation order working

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -15,15 +15,18 @@ export default function AboutMe() {
   const word1 = useRef(null)
   const word2 = useRef(null)
   const word3 = useRef(null)
+  const word4 = useRef(null)
 
 useEffect(() => {
   let word1Length=word1.current.textContent.length;
   let word2Length=word2.current.textContent.length;
   let word3Length=word3.current.textContent.length;
+  let word4Length=word3.current.textContent.length;
 
   console.log(word1Length)
   console.log(word2Length)
   console.log(word3Length)
+  console.log(word4Length)
 
 }, [word1, word2, word3])
   
@@ -54,9 +57,9 @@ useEffect(() => {
           
         <ul className='border-left text-center dynamic-text'>
           <li className='word1'><span ref={word1} className='dynamic-text word-1'>JavaScript Developer</span></li>
-          <li className='word1'><span ref={word2}  className='dynamic-text word-2'>React Developer</span></li>
-          <li className='word1'><span ref={word3}  className='dynamic-text word-3'>Musician</span></li>
-      
+          <li className='word2'><span ref={word2}  className='dynamic-text word-2'>React Developer</span></li>
+          <li className='word3'><span ref={word3}  className='dynamic-text word-3'>Musician</span></li>
+          <li className='word4'><span ref={word4}  className='dynamic-text word-4'>Software Engineer</span></li>
 
         </ul>
         </div>
